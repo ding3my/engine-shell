ac57aa5bab55373564841c782453caef4e8b2312
yjr <yjr@localhost.localdomain>
2017-03-08 14:30:33 +0800

    Wed Mar  8 14:30:33 CST 2017

---
+++ engine-shell.sh
@@ -0,0 +1,13 @@
+source ./config.sh
+
+function cdf(){
+#echo $#
+regexp=".*"
+for i in $@; do
+    regexp=${regexp}"$i"".*"
+done
+#echo "find \"$path_to_find\" -type d | grep -P \"$regexp\" | head -n1" # test
+cmd="cd '`find \"$path_to_find\" -type d | grep -P \"$regexp\" | head -n1`'"
+echo "$cmd"
+eval "$cmd"
+}
---
+++ git.sh
@@ -0,0 +1,1 @@
+alias gita='git add'
